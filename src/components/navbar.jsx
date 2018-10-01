3622f6a87f343df3b5836d0d92fa3d2a1de2ea1d
annmunyare <ann.munyare@nanyukiappfactory.onmicrosoft.com>
2018-10-01 20:10:43 +0300

    fixed some errors

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
-class navBar extends Component {
+class NavBar extends Component {
   render() {
     return (
       <nav class="navbar navbar-light bg-light">
-        totalCounters=
-        {this.state.counters.filter(c => c.value > 0).length}
         <a class="navbar-brand" href="#">
-          Navbar
+          Shop
         </a>
       </nav>
     );
   }
 }
 
-export default navBar;
-// stateles functions
-// const navBar = () => {
-//     return (  );
-// }
-
-// export default navBar;
+export default NavBar;
